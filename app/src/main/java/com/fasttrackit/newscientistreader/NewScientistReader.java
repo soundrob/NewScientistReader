ae3aacd77a45e4945f2a2236039a69fbf5de173e
soundrob <paltanrobert@yahoo.com>
2016-10-24 20:29:25 +0300

    New Scientiest Reader

---
+++ app/src/main/java/com/fasttrackit/newscientistreader/NewScientistReader.java
@@ -0,0 +1,17 @@
+package com.fasttrackit.newscientistreader;
+
+import android.app.Application;
+
+/**
+ * @author mihai.mecea
+ */
+
+public class NewScientistReader extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+  }
+
+}
